4ec55504a34a7324dbec18993bec189759c8f77d
Samantha <94052042+samanthag97@users.noreply.github.com>
2022-11-24 14:36:44 +0100

    added a new button that increases the value in the first fragment

--- app/src/main/java/com/example/veterinaria/FirstFragment.java
+++ app/src/main/java/com/example/veterinaria/FirstFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -14,14 +16,17 @@ import com.example.veterinaria.databinding.FragmentFirstBinding;
 public class FirstFragment extends Fragment {
 
     private FragmentFirstBinding binding;
+    TextView showCountTextView;
 
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
-
+        //le due righe successive vedono il valore di showCount e lo sostituiscono a textview:
+        //in questo caso, viene incrementato dal second button (nel primo fragment!)
         binding = FragmentFirstBinding.inflate(inflater, container, false);
+        showCountTextView = binding.textviewFirst.findViewById(R.id.textview_first);
         return binding.getRoot();
 
     }
@@ -29,6 +34,7 @@ public class FirstFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        //click listener for first button
         binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -36,6 +42,29 @@ public class FirstFragment extends Fragment {
                         .navigate(R.id.action_FirstFragment_to_SecondFragment);
             }
         });
+
+        //click listener for the second button ("lo clicco e fa qualcosa")
+        binding.buttonSecond.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                countMe(view);
+               //toast -> it prints a temp. string at the bottom of the first fragment
+               //Toast myToast = Toast.makeText(getActivity(), "Hello there", Toast.LENGTH_SHORT);
+               //myToast.show();
+            }
+        });
+    }
+
+    //il valore di textView viene memorizzato in una stringa,
+    //convertito in intero e incrementato
+    //riconvertito a stringa
+    private void countMe(View view) {
+        String countString = showCountTextView.getText().toString();
+        Integer count = Integer.parseInt(countString);
+        count++;
+        showCountTextView.setText(count.toString());
+
+
     }
 
     @Override
