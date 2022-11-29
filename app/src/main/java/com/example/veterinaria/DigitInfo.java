7e9acc6d690e2a188071a2a999a29996973bd56d
Samantha <94052042+samanthag97@users.noreply.github.com>
2022-11-29 13:12:10 +0100

    now we are able to send some text to firebase

---
+++ app/src/main/java/com/example/veterinaria/DigitInfo.java
@@ -0,0 +1,18 @@
+package com.example.veterinaria;
+
+public class DigitInfo {
+
+    private String digit;
+
+    public DigitInfo(){
+
+    }
+
+    public String getDigit() {
+        return digit;
+    }
+
+    public void setDigit(String digit) {
+        this.digit = digit;
+    }
+}
--- app/src/main/java/com/example/veterinaria/FirstFragment.java
+++ app/src/main/java/com/example/veterinaria/FirstFragment.java
@@ -4,8 +4,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.EditText;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -17,6 +25,13 @@ public class FirstFragment extends Fragment {
 
     private FragmentFirstBinding binding;
     TextView showCountTextView;
+    private EditText digitEdt;
+    private Button sendDataEdt;
+
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+
+    DigitInfo digitInfo;
 
     @Override
     public View onCreateView(
@@ -33,6 +48,24 @@ public class FirstFragment extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        digitEdt = view.findViewById(R.id.editTextDigit);
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference();
+
+        digitInfo = new DigitInfo();
+
+        binding.firebase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = digitEdt.getText().toString();
+                addDataToFirebase(name);
+
+            }
+        });
+
+
 
         //click listener for first button
         binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
@@ -65,6 +98,23 @@ public class FirstFragment extends Fragment {
             public void onClick(View view) {
                 NavHostFragment.findNavController(FirstFragment.this)
                         .navigate(R.id.action_FirstFragment_to_thirdFragment);
+            }
+        });
+    }
+
+    private void addDataToFirebase(String name) {
+        digitInfo.setDigit(name);
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                databaseReference.setValue(digitInfo);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
             }
         });
     }
